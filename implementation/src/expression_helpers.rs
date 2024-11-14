4bb81cb25e6468a095c7862ca2a4db5c55727048
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2024-11-13 21:42:17 -0500

    Implement `parent` helper

--- implementation/src/expression_helpers.rs
+++ implementation/src/expression_helpers.rs
@@ -82,6 +82,61 @@ pub fn height(expressions: &ExpressionArena, expression: ExpressionId) -> usize 
     ExpressionHeight::new(expressions).compute_expression_height(expression)
 }
 
+struct Parent<'a> {
+    expressions: &'a ExpressionArena,
+    parent: Vec<Option<ExpressionId>>,
+}
+
+impl<'a> Parent<'a> {
+    pub fn new(expressions: &'a ExpressionArena) -> Parent<'a> {
+        Parent {
+            expressions,
+            parent: vec![Option::None; expressions.len()],
+        }
+    }
+
+    fn visit(&mut self, expression: ExpressionId) {
+        match &self.expressions[expression] {
+            Expression::Variable { identifier: _ } => {}
+            Expression::NamelessVariable { index: _ } => {}
+            Expression::Abstraction { parameter: _, body } => {
+                self.parent[body.into_usize()] = Option::Some(expression.clone());
+                self.visit(*body);
+            }
+            Expression::NamelessAbstraction { body } => {
+                self.parent[body.into_usize()] = Option::Some(expression.clone());
+                self.visit(*body);
+            }
+            Expression::Application {
+                function,
+                arguments,
+            } => {
+                self.parent[function.into_usize()] = Option::Some(expression.clone());
+                self.visit(*function);
+                for &argument in arguments {
+                    self.parent[argument.into_usize()] = Option::Some(expression.clone());
+                    self.visit(argument);
+                }
+            }
+        }
+    }
+
+    pub fn compute_parent_expressions(
+        mut self,
+        expression: ExpressionId,
+    ) -> Vec<Option<ExpressionId>> {
+        self.visit(expression);
+        self.parent
+    }
+}
+
+pub fn parent_expressions(
+    expressions: &ExpressionArena,
+    expression: ExpressionId,
+) -> Vec<Option<ExpressionId>> {
+    Parent::new(expressions).compute_parent_expressions(expression)
+}
+
 struct IsNamed<'a> {
     expressions: &'a ExpressionArena,
 }
