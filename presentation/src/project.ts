bedbb4be9eed34c4744324587a27bd01a9f40203
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2024-11-14 22:58:23 -0500

    Add grammar slide

--- presentation/src/project.ts
+++ presentation/src/project.ts
@@ -2,10 +2,11 @@ import { makeProject } from "@motion-canvas/core";
 
 import title from "./scenes/title?scene";
 import motivation from "./scenes/motivation?scene";
+import grammar from "./scenes/grammar?scene";
 
 import "./global.css";
 
 export default makeProject({
   name: "Variable Name Generation Tutorial",
-  scenes: [title, motivation],
+  scenes: [title, motivation, grammar],
 });
---
+++ presentation/src/scenes/grammar.tsx
@@ -0,0 +1,55 @@
+import { Latex, makeScene2D } from "@motion-canvas/2d";
+import {
+  beginSlide,
+  createRef,
+  Direction,
+  slideTransition,
+} from "@motion-canvas/core";
+import { backgroundFill } from "../constants";
+
+export default makeScene2D(function* (view) {
+  view.fill(backgroundFill);
+
+  const grammar = createRef<Latex>();
+  view.add(
+    <Latex
+      ref={grammar}
+      tex={[
+        "\\text{Terms}\\ ",
+        "M, N",
+        "::=",
+        "x",
+        "\\mid",
+        "{\\lambda x. M}",
+        "\\mid",
+        "{M\\ N_1\\ N_2 \\cdots N_k}",
+      ]}
+      fill="white"
+      fontSize={54}
+    />
+  );
+
+  yield* slideTransition(Direction.Right);
+
+  yield* beginSlide("named-grammar");
+
+  yield* grammar().tex(
+    [
+      "\\text{Terms}\\ ",
+      "M, N",
+      "::=",
+      "x",
+      "\\mid",
+      "{\\lambda x. M}",
+      "\\mid",
+      "{M\\ N_1\\ N_2 \\cdots N_k}",
+      "\\mid",
+      "{\\color{cyan}{\\iota}}",
+      "\\mid",
+      "{\\color{cyan}{{\\lambda. M}}}",
+    ],
+    1
+  );
+
+  yield* beginSlide("nameless-grammar");
+});
