6e94b69e9544c69aab1649e6e023718eb794f152
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2024-11-14 22:57:54 -0500

    Add motivation slide

--- presentation/src/project.ts
+++ presentation/src/project.ts
@@ -1,10 +1,11 @@
 import { makeProject } from "@motion-canvas/core";
 
 import title from "./scenes/title?scene";
+import motivation from "./scenes/motivation?scene";
 
 import "./global.css";
 
 export default makeProject({
   name: "Variable Name Generation Tutorial",
-  scenes: [title],
+  scenes: [title, motivation],
 });
---
+++ presentation/src/scenes/motivation.tsx
@@ -0,0 +1,42 @@
+import { Layout, makeScene2D, Txt } from "@motion-canvas/2d";
+import {
+  all,
+  beginSlide,
+  createRef,
+  Direction,
+  slideTransition,
+} from "@motion-canvas/core";
+
+import { backgroundFill } from "../constants";
+
+export default makeScene2D(function* (view) {
+  view.fill(backgroundFill);
+
+  view.add(
+    <Layout direction={"column"} gap={40} width={1920} padding={100} layout>
+      <Txt fontFamily={"Roboto"} fill={"white"} fontSize={100}>
+        Motivation
+      </Txt>
+      <Txt fontFamily={"Roboto"} fill={"white"} textWrap={true}>
+        Variable name generation occurs whenever synthesized terms have to be
+        pretty-printed:
+      </Txt>
+      <Txt fontFamily={"Roboto"} fill={"white"}>
+        • After code snippet generation for splicing to the editor's buffer
+      </Txt>
+      <Txt fontFamily={"Roboto"} fill={"white"}>
+        • After type inference for error-reporting
+      </Txt>
+      <Txt fontFamily={"Roboto"} fill={"white"}>
+        • After term reconstruction for implicit arguments
+      </Txt>
+      <Txt fontFamily={"Roboto"} fill={"white"}>
+        • After automated proof search for synthesized programs
+      </Txt>
+    </Layout>
+  );
+
+  yield* slideTransition(Direction.Right);
+
+  yield* beginSlide("motivation");
+});
